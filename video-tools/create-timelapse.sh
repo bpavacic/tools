4d88e9d37d3d30cb9fdc4163f52df16989f0a2a0
Boris Pavacic <boris.pavacic@gmail.com>
2020-08-04 21:47:28 +0700

    Added optional parameter to specify the speed up factory

--- video-tools/create-timelapse.sh
+++ video-tools/create-timelapse.sh
@@ -7,18 +7,47 @@
 # Abort on any error.
 set -e
 
-if [ "$#" -ne 2 ]; then
-    echo "Error: Wrong number of arguments"
-    echo ""
-    echo "Usage: $0 <input_file> <output_file>"
-    exit 1
-fi
+FACTOR=16
 
-INPUT=$1
-OUTPUT=$2
+while test $# -gt 0
+do
+    case "$1" in
+      -f)
+          FACTOR=$2
+          shift
+          shift
+          ;;
+      *) 
+          if [ -z "$INPUT" ];
+          then
+          	INPUT=$1
+          else
+     	     if [ -z "$OUTPUT" ];
+     	     then
+     	     	OUTPUT="$1"
+     	     	args_ok=1
+     		 else
+     		 	args_ok=0
+     		 fi
+          fi
+          shift
+          ;;
+    esac
+done
+
+if ! [[ "$args_ok" == "1" ]];
+then
+	echo ""
+	echo "Error: invalid arguments"
+	echo ""
+	echo "Usage: $0 <file1> <file2> [options] "
+	echo "Options:"
+	echo "  -f speedup_factor (defaults to 16)"
+	exit 1
+fi
 
 # -movflags +faststart => speeds up video startup by storing bunch of metadata at the beginning of the file
 # map_metadata 0 => Copies mp4 metadata (such as location of the video, time recorded etc) from the first input file
 MPEG_OPTIONS="-movflags +faststart -map_metadata 0"
 
-ffmpeg -i "$INPUT" -vf "setpts=PTS/16" -an $MPEG_OPTIONS "$OUTPUT"
+ffmpeg -i "$INPUT" -vf "setpts=PTS/$FACTOR" -an $MPEG_OPTIONS "$OUTPUT"
